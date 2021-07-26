92fd3cdb9a5abcd020adb009e2821ee486145a82
kiloev <freakperzon@yahoo.com>
2021-07-26 22:17:17 +0700

    Migrate again to Sanic

---
+++ api/server.py
@@ -0,0 +1,22 @@
+from sanic import Sanic
+from sanic.response import empty, json
+
+from api import _utils
+
+
+app = Sanic(__name__)
+
+
+@app.post('/bot')
+async def bot(request):
+    token = request.args.get('token')
+    message = request.json.get('message', {})
+    text = message.get('text') or ''
+    chat_id = message.get('chat', {}).get('id')
+
+    reply = _utils.get_reply(text)
+    if token and chat_id:
+        _utils.send_reply(token, chat_id, reply)
+        return empty()
+
+    return json(reply)
